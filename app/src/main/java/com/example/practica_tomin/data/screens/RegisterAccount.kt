ac2421267430cf85be6a7b10c2a9a048e82dc5ac
Tomin <tomin8873@mail.ru>
2025-12-15 13:26:16 +0300

    Готовый экран Регистрации

--- app/src/main/java/com/example/practica_tomin/data/screens/RegisterAccount.kt
+++ app/src/main/java/com/example/practica_tomin/data/screens/RegisterAccount.kt
@@ -1,25 +1,14 @@
 package com.example.practice_mobile.ui.screen
 
-import androidx.compose.material3.Surface
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,38 +20,53 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.practica_tomin.R
 
-//Cоздание экрана регистрации
+// Создание экрана регистрации
 @Composable
-fun RegisterAccount() {
+fun RegisterAccount(
+    onBackClick: () -> Unit = {},
+    onLoginClick: () -> Unit = {},
+    onRegisterClick: (String, String, String) -> Unit = { _, _, _ -> }
+) {
     val name = remember { mutableStateOf("") }
     val email = remember { mutableStateOf("") }
     val password = remember { mutableStateOf("") }
     val passwordVisible = remember { mutableStateOf(false) }
     val agreementChecked = remember { mutableStateOf(false) }
 
+
+    val isFormValid =
+        name.value.isNotBlank() &&
+                email.value.isNotBlank() &&
+                password.value.isNotBlank() &&
+                agreementChecked.value
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 24.dp),
+                .padding(horizontal = 24.dp)
+                .padding(top = 16.dp)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(Modifier.height(24.dp))
-
             // Верхняя панель с кнопкой Back
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 22.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                IconButton(onClick = { /* TODO back */ }) {
+                IconButton(onClick = onBackClick) {
                     Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Назад"
+                        painter = painterResource(id = R.drawable.icon),
+                        contentDescription = "Назад",
+                        tint = Color.Unspecified
                     )
                 }
             }
@@ -71,31 +75,54 @@ fun RegisterAccount() {
 
             Text(
                 text = "Регистрация",
-                style = MaterialTheme.typography.headlineMedium
+                style = MaterialTheme.typography.headlineMedium,
+                fontSize = 32.sp
             )
             Text(
                 text = "Заполните Свои данные",
                 style = MaterialTheme.typography.bodyMedium,
-                color = Color.Gray
+                color = Color(0xFF707B81),
+                fontSize = 16.sp
             )
 
             Spacer(Modifier.height(32.dp))
 
             // Имя
-            Text(text = "Ваше имя", style = MaterialTheme.typography.bodyMedium)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = "Ваше имя",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontSize = 16.sp,
+
+                )
+            }
             OutlinedTextField(
                 value = name.value,
                 onValueChange = { name.value = it },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 8.dp),
-                placeholder = { Text("xxxxxxxx") }
+                placeholder = { Text("xxxxxxxx") },
+                singleLine = true,
+                shape = RoundedCornerShape(16.dp)
             )
 
             Spacer(Modifier.height(16.dp))
 
-            // Email
-            Text(text = "Email", style = MaterialTheme.typography.bodyMedium)
+// Email
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = "Email",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontSize = 16.sp
+                )
+            }
             OutlinedTextField(
                 value = email.value,
                 onValueChange = { email.value = it },
@@ -103,17 +130,30 @@ fun RegisterAccount() {
                     .fillMaxWidth()
                     .padding(top = 8.dp),
                 placeholder = { Text("xyz@gmail.com") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                singleLine = true,
+                shape = RoundedCornerShape(16.dp)
             )
 
             Spacer(Modifier.height(16.dp))
 
-            // Пароль
-            Text(text = "Пароль", style = MaterialTheme.typography.bodyMedium)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = "Пароль",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontSize = 16.sp
+                )
+            }
             OutlinedTextField(
                 value = password.value,
                 onValueChange = { password.value = it },
-                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
                 visualTransformation = if (passwordVisible.value)
                     VisualTransformation.None
                 else
@@ -123,9 +163,9 @@ fun RegisterAccount() {
                         Icon(
                             painter = painterResource(
                                 id = if (passwordVisible.value)
-                                    R.drawable.union
+                                    R.drawable.eye_open     // иконка «глаз закрыт»
                                 else
-                                    R.drawable.eye_open
+                                    R.drawable.union  // иконка «глаз открыт»
                             ),
                             contentDescription = if (passwordVisible.value)
                                 "Скрыть пароль"
@@ -134,30 +174,55 @@ fun RegisterAccount() {
                         )
                     }
                 },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                singleLine = true
             )
 
             Spacer(Modifier.height(16.dp))
 
-            // Чекбокс согласия
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Checkbox(
-                    checked = agreementChecked.value,
-                    onCheckedChange = { agreementChecked.value = it }
+                Icon(
+                    painter = painterResource(
+                        id = if (agreementChecked.value)
+                            R.drawable.frame_1000000814__1_
+                        else
+                            R.drawable.policy_check
+                    ),
+                    contentDescription = if (agreementChecked.value)
+                        "Отмечено"
+                    else
+                        "Не отмечено",
+                    tint = Color.Unspecified,
+                    modifier = Modifier
+                        .size(20.dp)
+                        .clickable {
+                            agreementChecked.value = !agreementChecked.value
+                        }
                 )
+
                 Text(
-                    fontSize = 14.sp,
+                    fontSize = 18.sp,
                     text = "Даю согласие на обработку\nперсональных данных",
-                    style = MaterialTheme.typography.bodySmall
+                    color = Color.Gray,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        textDecoration = TextDecoration.Underline
+                    )
                 )
             }
 
             Spacer(Modifier.height(24.dp))
 
             Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF48B2E7),
+                    contentColor = Color.White,
+
+                    disabledContainerColor = Color(0xFF2B6B8B),
+                    disabledContentColor = Color.White
+                ),
                 onClick = { /* TODO: регистрация */ },
                 enabled = agreementChecked.value,
                 modifier = Modifier
@@ -177,7 +242,7 @@ fun RegisterAccount() {
                 Text(text = "Есть аккаунт? ")
                 Text(
                     text = "Войти",
-                    color = Color(0xFF0077B6),
+                    color = Color(0xFF000000),
                     fontWeight = FontWeight.Medium,
                     modifier = Modifier.clickable { /* TODO: навигация к логину */ }
                 )
